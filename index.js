6bd6e6c1e91cdda46fa8bbf3eca35d42eeab1a03
ayush gupta <ayushgupta11442@gmail.com>
2023-07-14 14:54:18 +0530

    learned the basic module in nodejs

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const name = "ayush";
-console.log("ayush");
+// const name = "ayush";
+// console.log("ayush");
+
+const fs = require("fs");
+
+// fs.writeFileSync('read.txt' , "this is first cource")
+
+// fs.appendFileSync('read.txt' , "yes i am doing great");
+
+
+
+const buf_data = fs.readFileSync('read.txt');
+
+console.log(buf_data);
+
+org_data = buf_data.toString();
+
+console.log(org_data);
+
+fs.renameSync("read.txt" , "read1.txt");
