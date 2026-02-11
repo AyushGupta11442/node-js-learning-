79fb69338e7a29ac19ae645c3a34f7d4fb2cc79b
AyushGupta11442 <ayushgupta11442@gmail.com>
2026-02-11 21:43:56 +0530

    nothing

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-// const name = "ayush";
-// console.log("ayush");
+var name =  "Ayush";
 
-const fs = require("fs");
+var a = 10;
 
-// fs.writeFileSync('read.txt' , "this is first cource")
+console.log(name);
 
-// fs.appendFileSync('read.txt' , "yes i am doing great");
-
-
-
-const buf_data = fs.readFileSync('read.txt');
-
-console.log(buf_data);
-
-org_data = buf_data.toString();
-
-console.log(org_data);
-
-fs.renameSync("read.txt" , "read1.txt");
+// console.log(global);
+console.log(this);
